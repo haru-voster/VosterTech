5ed530aa9e0d73ec33a7ac6a3ca2f122c5f0fe18
haru-voster <harounbravo2@gmail.com>
2025-11-24 15:34:57 +0300

    products

--- js/script.js
+++ js/script.js
@@ -356,7 +356,7 @@ const products = [
     type: "display", // matches dropdown "Disply"
     name: "HDMI 1.5M LONG",
     price: "Ksh 500",
-    desc: "4K HDMI Cable 1.5 Metres, JSAUX 18Gbps High Speed HDMI 2.0 Braided HDMI Cord, 4K 60Hz",
+    desc: "4K HDMI Cable 1.5 Metres, High Speed HDMI 2.0 Braided HDMI Cord, 4K 60Hz",
     img: "img/HDMI.png"
   }
   ,
